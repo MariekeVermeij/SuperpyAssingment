d4cdb7da0c1e5617df9df5cee4024662c7b4d791
MariekeVermeij <86728312+MariekeVermeij@users.noreply.github.com>
2022-02-28 20:39:26 +0100

    Add files via upload

--- set_date.py
+++ set_date.py
@@ -1,7 +1,16 @@
 import  datetime 
+import csv
+
 # Current date,  yesterday's date in variables
-today = datetime.datetime.today().strftime("%Y-%m-%d")
-yesterday = (datetime.datetime.today() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+def get_date():
+    with open ('today.csv', mode='r',encoding='utf8',newline='') as today_file:
+            reader_today = csv.DictReader(today_file,delimiter = ';',lineterminator='\n', quotechar='"')
+            for row in reader_today:
+                date_to_use = (row['today_date'])
+                return date_to_use
+
+today = get_date()
+yesterday = (datetime.datetime.strptime(today, '%Y-%m-%d') - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
 
 
 
